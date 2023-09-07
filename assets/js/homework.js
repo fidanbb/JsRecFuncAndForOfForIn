54a66dcebe8ee822eb0e4f4337ba1ce85579217d
Fidan Bashirova <fidanbb@code.edu.az>
2023-09-07 19:14:57 +0400

    added homework

---
+++ assets/js/homework.js
@@ -0,0 +1,70 @@
+"use strict";
+
+//2) Icerisinde name, surname, age, email olan bir nece student objectleriniz var. Ashagida elave etdiklerimi functionlar sekilinde yazin,      functionlar objectlerden ibaret array qebul edecek.
+// 1) Yashi 18-25 araliginda olan telebelerin sayi.
+// 2) Emailinde "c" herfi olan studentlerin sayi.
+
+function filter(students) {
+  for (const student of students) {
+    if (student.age > 18 && student.age < 25) {
+      for (const key in student) {
+        console.log(key + "---" + student[key]);
+      }
+    }
+  }
+}
+
+function count(students) {
+  let count = 0;
+  for (const student of students) {
+    if (student.email) {
+      let email = student.email;
+      for (const key in email) {
+        if (email[key] === "c") {
+          count++;
+        }
+      }
+    }
+  }
+  console.log(count);
+}
+
+let stu1 = {
+  name: "Fidan",
+  surname: "Bashirova",
+  age: 22,
+  email: "fidan@gmail.com",
+};
+
+let stu2 = {
+  name: "Nigar",
+  surname: "Adilova",
+  age: 30,
+  email: "nigar@gmail.com",
+};
+
+let stu3 = {
+  name: "Eli",
+  surname: "Eliyev",
+  age: 17,
+  email: "eli@gmail.com",
+};
+
+let stu4 = {
+  name: "Rustem",
+  surname: "Rustemov",
+  age: 24,
+  email: "fidan@gmail.com",
+};
+
+let stu5 = {
+  name: "Zehra",
+  surname: "Babayev",
+  age: 23,
+  email: "zehra@gmail.com",
+};
+
+let students = [stu1, stu2, stu3, stu4, stu5];
+
+// filter(students);
+// count(students);
---
+++ assets/js/script.js
@@ -0,0 +1,114 @@
+"use strict";
+
+// function showNums(n) {
+//   for (let i = 0; i <= n; i++) {
+//     console.log(i);
+//   }
+// }
+
+// showNums(5);
+
+// function showNums(num) {
+//   debugger;
+//   let number = num - 1;
+//   console.log(num);
+
+//   if (number > 0) {
+//     showNums(number);
+//   }
+// }
+
+// showNums(5);
+
+// function sum(m) {
+//   let sum = 0;
+//   for (let i = 0; i <= m; i++) {
+//     sum += i;
+//   }
+//   console.log(sum);
+// }
+
+// sum(4);
+
+// function sumOfNums(m) {
+//   if (m == 0) {
+//     return m;
+//   } else {
+//     return m + sumOfNums(m - 1);
+//   }
+// }
+
+// console.log(sumOfNums(4));
+
+// let num = 5 + test(2, 4);
+
+// function test(a, b) {
+//   return a + b + test2();
+// }
+
+// function test2() {
+//   return 50;
+// }
+
+// console.log(num);
+
+// let names = ["Fidan", "Kubra", "Cahangir", "Ismayil"];
+
+// for (const item of names) {
+//   console.log(item);
+// }
+
+// let student = {
+//   name: "Xeyyam",
+//   surname: "Isgendorov",
+//   age: 29,
+//   address: "Yasamal",
+// };
+
+// for (const key in student) {
+//   //   console.log(key + "---" + student[key]);
+
+//   if (key == "age") {
+//     console.log("yes");
+//     break;
+//   }
+//   //   else {
+//   //     console.log("no");
+//   //   }
+// }
+
+// let stu1 = {
+//   name: "Xeyyam",
+//   age: 29,
+// };
+
+// let stu2 = {
+//   name: "Pervin",
+//   age: 30,
+// };
+// let stu3 = {
+//   name: "Fidan",
+//   age: 22,
+// };
+
+// let stu4 = {
+//   name: "Kubra",
+//   age: 25,
+// };
+// let stu5 = {
+//   name: "Surac",
+//   age: 23,
+// };
+
+// let students = [stu1, stu2, stu3, stu4, stu5];
+
+// for (const student of students) {
+//   //   if (student.name == "Xeyyam")
+
+//   if (student.age > 25) {
+//     for (const key in student) {
+//       console.log(key + "---" + student[key]);
+//     }
+//     // break;
+//   }
+// }
